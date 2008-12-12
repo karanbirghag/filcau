47292a5461343d16031b435d0d5697cf6b45b399
nistor.i.andrei <nistor.i.andrei@c03680aa-d449-0410-87ef-d190aa3f368e>
2008-12-12 08:44:02 +0000

    Store class

---
+++ Mallus/src/mallus/model/Store.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mallus.model;
+
+/**
+ *
+ * @author andrei.nistor
+ */
+public abstract class Store {
+    protected abstract Store createStore();
+}
