0fc91bc5ab7ce74f9ae43f62aab04eb0601918d8
nistor.i.andrei <nistor.i.andrei@c03680aa-d449-0410-87ef-d190aa3f368e>
2008-12-12 11:51:46 +0000

    [AN] add commit class

---
+++ Mallus/src/mallus/model/Item.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mallus.model;
+
+/**
+ *
+ * @author andrei.nistor
+ */
+public class Item {
+
+    private String name;
+    private String id;
+    private String storeId;
+    private Double price;
+
+    public Item(String name, String id, String storeId) {
+        this.name = name;
+        this.id = id;
+        this.storeId = storeId;
+    }
+
+    public Item(String name, String id, String storeId, Double price) {
+        this(name, id, storeId);
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(String storeId) {
+        this.storeId = storeId;
+    }
+    
+}
