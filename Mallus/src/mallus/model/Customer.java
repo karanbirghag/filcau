94e6d241478e0fb202b9f675cc9a056416f88ae3
mihai.enea <mihai.enea@c03680aa-d449-0410-87ef-d190aa3f368e>
2008-12-12 09:55:21 +0000

    ME Add new classes. Methods will be added later.

---
+++ Mallus/src/mallus/model/Customer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mallus.model;
+
+/**
+ *
+ * @author mihai.enea
+ */
+class Customer {
+
+    public ShoppingCart shoppingCart;
+    public String name;
+    public Store store;
+    
+}
---
+++ Mallus/src/mallus/model/Mall.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mallus.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author mihai.enea
+ */
+public class Mall {
+
+   static Mall mallInstance;
+   public String name;
+   public List <Store> stores;
+   public List <Customer> customers;
+
+   private Mall() {
+   }
+
+   public static Mall getInstance() {
+      if (null == mallInstance) {
+         mallInstance = new Mall();
+      }
+      return mallInstance;
+   }
+
+   protected Object clone() throws
+    CloneNotSupportedException {
+      throw new CloneNotSupportedException();
+   }
+
+   
+}
