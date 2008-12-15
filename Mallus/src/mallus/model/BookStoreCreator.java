9a4ebe3638e31344d60101ce00289250494c9812
mariuslazar <mariuslazar@c03680aa-d449-0410-87ef-d190aa3f368e>
2008-12-15 15:35:52 +0000

    [ML] new items

--- Mallus/src/mallus/model/BookStoreCreator.java
+++ Mallus/src/mallus/model/BookStoreCreator.java
@@ -30,11 +30,11 @@ public class BookStoreCreator extends StoreCreator {
      */
     private Enumeration<Item> getItemsForStore() {
         Enumeration<Item> items = new ItemsEnumeration<Item>();
-        Item i1 = new Item("Item1", "Item1_BookStore", BookStoreCreator.SHOP_NAME, new Double(2.0));
-        Item i2 = new Item("Item2", "Item2_BookStore", BookStoreCreator.SHOP_NAME, new Double(2.3));
-        Item i3 = new Item("Item3", "Item3_BookStore", BookStoreCreator.SHOP_NAME, new Double(1.1));
-        Item i4 = new Item("Item4", "Item4_BookStore", BookStoreCreator.SHOP_NAME, new Double(19.2));
-        Item i5 = new Item("Item5", "Item5_BookStore", BookStoreCreator.SHOP_NAME, new Double(5.5));
+        Item i1 = new Item("Memories from the childhood", "Item1_BookStore", BookStoreCreator.SHOP_NAME, new Double(2.0));
+        Item i2 = new Item("The bag with two coins", "Item2_BookStore", BookStoreCreator.SHOP_NAME, new Double(2.3));
+        Item i3 = new Item("The goat with the three kids", "Item3_BookStore", BookStoreCreator.SHOP_NAME, new Double(1.1));
+        Item i4 = new Item("The bear tricked by the fox", "Item4_BookStore", BookStoreCreator.SHOP_NAME, new Double(19.2));
+        Item i5 = new Item("The story of a lazy man", "Item5_BookStore", BookStoreCreator.SHOP_NAME, new Double(5.5));
         items.add(i1);
         items.add(i2);
         items.add(i3);
