4e1101de72a36ce5d2bf7a692973501f1cd989a4
grozavpava <grozavpava@c03680aa-d449-0410-87ef-d190aa3f368e>
2009-03-10 22:10:26 +0000

    Implement joining teams

--- Filcau/src/resources/js/gr.js
+++ Filcau/src/resources/js/gr.js
@@ -23,16 +23,22 @@ function updatePlayerList() {
 			alert("The game was ended by the owner.");
 			window.location.replace("/");
 		}
-		players = JSON.parse(data);
+		response = JSON.parse(data);
+		players = response["players"];
 		var s = "";
 		for (index in players) {
-			s += "<p>" + players[index] + " <span class='small'>not ready</span>" + "</p>";
+			s += "<p><label>" + players[index] + "</label> <span class='small valid'>NOT READY</span>" + "</p>";
 		} 
 		$("#players").html(s);
 
+		displayTeams(response["teams"]);
 		if (players.length == 4) {
 			$("#status").text("Game room is full");
-			$("#startGameBt").show();
+			if ($(".invalid").size() == 0) {
+				$("#startGameBt").show();
+			} else {
+				$("#startGameBt").hide();
+			}
 		} else {
 			$("#status").text("Waiting for players...");
 			$("#startGameBt").hide();
@@ -41,7 +47,43 @@ function updatePlayerList() {
 };
 
 function doJoin(team) {
-	$.post('/g', {op: 'joinTeam', id: gameId, team: team}, function(data) {
-		alert(data);
+	$("#teams a.join").addClass("hidden");
+	$("#team" + team + " a.leave").removeClass("hidden");
+	$.post('/g', {op: 'joinTeam', id: gameId, team: team}, loadTeams);
+};
+function doLeaveTeam(team) {
+	$("#teams a.join").removeClass("hidden");
+	$("#team" + team + " a.leave").addClass("hidden");
+	$.post('/g', {op: 'leaveTeam', id: gameId, team: team}, loadTeams);
+};
+function loadTeams(data) {
+		teams = JSON.parse(data);
+		displayTeams(teams);
+};
+function displayTeams(teams) {
+	for (index in teams) {
+		var player = $("#player" + index);		
+		if (teams[index] == null) {
+			player.removeClass("active");
+		} else {
+			player.text(teams[index]);
+			player.addClass("active");
+		}
+	}
+	updatePlayerStatus();
+};
+
+function updatePlayerStatus() {
+	var players = new Array(); 
+	$("#teams div.player.active").each(function() {
+		players.push($(this).text());
+	});
+	$("#players p").each(function()  {
+	    jThis = $(this);
+	    if (jQuery.inArray(jThis.find("label").text(), players) == -1) {
+	        jThis.find("span").text("NOT READY").attr("class", "small invalid");
+	    } else {
+	    	jThis.find("span").text("READY").attr("class", "small valid");
+	    }
 	});
 };
