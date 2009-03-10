2d72810add2b873377ce2550d7847c253c30586a
grozavpava <grozavpava@c03680aa-d449-0410-87ef-d190aa3f368e>
2009-03-10 22:09:57 +0000

    Implement joining teams

--- Filcau/src/domain/model.py
+++ Filcau/src/domain/model.py
@@ -1,21 +1,50 @@
 from google.appengine.ext import db
 from google.appengine.api import users
 
+
 class Player(db.Model):
     user = db.UserProperty()
     alias = db.StringProperty()
 
 class Team(db.Model):
     name = db.StringProperty()
-    player1 = db.IntegerProperty()
-    player2 = db.IntegerProperty()
+    player1 = db.ReferenceProperty(Player, collection_name="teams1")
+    player2 = db.ReferenceProperty(Player, collection_name="teams2")
+
+    def addPlayer(self, player):
+        retValue = None
+        if not self.player1:
+            self.player1 = player
+            retValue = 1
+        elif not self.player2:
+            self.player2 = player
+            retValue = 2
+        return retValue
+
+    def removePlayer(self, player):
+        if self.player1 and self.player1.alias == player.alias:
+            self.player1 = None
+        elif self.player2 and self.player2.alias == player.alias:
+            self.player2 = None
+
+    def getPlayer1Alias(self):
+        if self.player1:
+            return self.player1.alias
+        else:
+            return None
+
+    def getPlayer2Alias(self):
+        if self.player2:
+            return self.player2.alias
+        else:
+            return None
 
 class Game(db.Model):
     name = db.StringProperty()
     creator = db.ReferenceProperty(Player)
     players = db.ListProperty(str)
-    team1 = Team(name='Team 1', player1=-1, player2=-1)
-    team2 = Team(name='Team 2', player1=-1, player2=-1)
+    team1 = db.ReferenceProperty(Team, collection_name="games1")
+    team2 = db.ReferenceProperty(Team, collection_name="games2")
 
     def addPlayer(self, player):
         if len(self.players) < 4:
@@ -29,20 +58,4 @@ class Game(db.Model):
         return i
     def playersConnected(self):
         return len(self.players)
-    def joinTeam(self, player, teamNumber):
-        team = self.team1
-        if teamNumber == 2:
-            team = self.team2
-        try:
-            i = self.players.index(str(player.key()))
-        except ValueError:
-            i = -1
-        if team.player1 == -1:
-            team.player1 = i
-            return 1
-        if team.player2 == -1:
-            team.player2 = i
-            return 2
-        return -1
-        
         
