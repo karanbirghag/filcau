d6272bd38d56fede377bee25d66f737a11146a51
munteanuliviuandrei@gmail.com <munteanuliviuandrei@gmail.com@c03680aa-d449-0410-87ef-d190aa3f368e>
2012-10-25 18:55:42 +0000

    error message JsonUtils.class

--- trunk/Filcau/branches/fedex2013/Parkingsquare/src/com/betfair/blocantii/LoginServlet.java
+++ trunk/Filcau/branches/fedex2013/Parkingsquare/src/com/betfair/blocantii/LoginServlet.java
@@ -10,28 +10,26 @@ import javax.servlet.http.HttpServletResponse;
 import com.betfair.blocantii.control.UserController;
 import com.betfair.blocantii.meta.UserMeta;
 import com.betfair.blocantii.model.User;
+import com.betfair.blocantii.util.Constants;
+import com.betfair.blocantii.util.JsonUtils;
 
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
 
 	private UserController userController = new UserController();
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 
 		String carNumber = req.getParameter("carNumber");
 		User user = userController.getUserByCarNumber(carNumber);
 
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hi " + user.getUsername());
-		
-		resp.setContentType("application/json");
-		// Get the printwriter object from response to write the required json object to the output stream      
-		PrintWriter out = resp.getWriter();
-		// Assuming your json object is **jsonObject**, perform the following, it will return your json object  
-		out.print(UserMeta.get().modelToJson(user));
-		out.flush();
-		
-	}
+		String output = user != null ? UserMeta.get().modelToJson(user)
+				: JsonUtils.errorToJson(Constants.ERR_NO_USER_FOR_CAR_NUMBER);
 
+		resp.setContentType("application/json");
+		PrintWriter out = resp.getWriter();
+		out.print(output);
+		out.flush();
+	}
 }
