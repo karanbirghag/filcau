d9096e798115b58261a939daa49342929a989227
munteanuliviuandrei@gmail.com <munteanuliviuandrei@gmail.com@c03680aa-d449-0410-87ef-d190aa3f368e>
2012-10-25 19:00:41 +0000

    add

---
+++ trunk/Filcau/branches/fedex2013/Parkingsquare/src/com/betfair/blocantii/util/Constants.java
@@ -0,0 +1,8 @@
+package com.betfair.blocantii.util;
+
+public final class Constants {
+
+	public final static String ERROR = "ERROR";
+	public final static String ERR_NO_USER_FOR_CAR_NUMBER = "NO_USER_FOR_CAR_NUMBER";
+
+}
